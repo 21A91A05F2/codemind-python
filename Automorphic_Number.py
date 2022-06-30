4be52fc9b5ce1216fa29cc13dfeb3814b58e6b9f
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-06-30 20:00:02 +0530

    def isAutomorphic(N) :     sq = N * N     while (N >0):         if (N % 10 != sq % 10) :             return False         N //= 10         sq //= 10     return True N = int(input()) if isAutomorphic(N) :     print ("Automorphic Number") else :     print  ("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,13 +1,13 @@
-n=int(input())
-sq=n*n
-flag=1
-while n:
-    if(sq%10!=n%10):
-        flag=0
-        break
-    n//=10
-    sq//=10
-if(flag==1):
-    print("Automorphic Number")
-if(flag==0):
-    print("Not an Automorphic Number")
+def isAutomorphic(N) :
+    sq = N * N
+    while (N >0):
+        if (N % 10 != sq % 10) :
+            return False
+        N //= 10
+        sq //= 10
+    return True
+N = int(input())
+if isAutomorphic(N) :
+    print ("Automorphic Number")
+else :
+    print  ("Not an Automorphic Number")
