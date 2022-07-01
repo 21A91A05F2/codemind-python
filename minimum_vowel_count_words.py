68c25383e2eaa73533aca82e79bbaa28425b7c5d
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-07-01 23:04:02 +0530

    n=input() v="aeiouAEIOU" c=0 a=[] for i in n:     if i==' ':         a.append(c)
            c=0
           
        elif i in v:
            c=c+1
    a.append(c)
    #print(a)
    if len(a)!=0:
        print(a.count(min(a)))
    else:
        print('-1')

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,19 @@
+
+n=input()
+v="aeiouAEIOU"
+c=0
+a=[]
+for i in n:
+    if i==' ':
+        a.append(c)
+        
+        c=0
+       
+    elif i in v:
+        c=c+1
+a.append(c)
+#print(a)
+if len(a)!=0:
+    print(a.count(min(a)))
+else:
+    print('-1')
