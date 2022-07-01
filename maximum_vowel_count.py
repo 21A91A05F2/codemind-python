2db41d61dbcc33dc6403469486cd54aac3d9d0f7
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-07-01 23:08:51 +0530

    n=input() v="aeiouAEIOU" c=0 a=[] for i in n:     if i==' ':         a.append(c)
            c=0
           
        elif i in v:
            c=c+1
    a.append(c)
    #print(a)
    if len(a)!=0:
        print(max(a))
    else:
        print('-1')

---
+++ maximum_vowel_count.py
@@ -0,0 +1,18 @@
+n=input()
+v="aeiouAEIOU"
+c=0
+a=[]
+for i in n:
+    if i==' ':
+        a.append(c)
+        
+        c=0
+       
+    elif i in v:
+        c=c+1
+a.append(c)
+#print(a)
+if len(a)!=0:
+    print(max(a))
+else:
+    print('-1')
