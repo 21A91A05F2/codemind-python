f8f368485144c43ff779770003408226783176e5
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-07-02 17:37:24 +0530

    def reverse(s):   str = ""   for i in s:     str = i + str   return str
    s =input()
      
    print (end="")
    print (reverse(s))

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -1,7 +1,11 @@
-def rev(s):
-    str=""
-    for i in s:
-        str=i+str
-    return str
-s=input()
-print(rev(s))
+
+def reverse(s):
+  str = ""
+  for i in s:
+    str = i + str
+  return str
+  
+s =input()
+  
+print (end="")
+print (reverse(s))
