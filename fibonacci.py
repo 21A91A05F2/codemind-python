9d249580d21cf6924cdfe20ffc94d29404408bc0
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-09-06 20:16:31 +0530

    def fun(n):     if n<0:         print("Incorrect Expression")     elif n==0:         return 0     elif n==1:         return 1     else:         return fun(n-1)+fun(n-2) n = int(input()) for i in range(n):     print(fun(i),end=" ")

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,12 @@
-def fab(n):
-    a,b=0,1
-    print(a,b,end=' ')
-    i=2
-    while i<n:
-        c=a+b
-        print(c,end=' ')
-        a=b
-        b=c
-        i+=1
-n=int(input())
-fab(n)
+def fun(n):
+    if n<0:
+        print("Incorrect Expression")
+    elif n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fun(n-1)+fun(n-2)
+n = int(input())
+for i in range(n):
+    print(fun(i),end=" ")
