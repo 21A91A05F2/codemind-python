47eafd4da6a9cdef39f66c2bc66a69fa577cf15b
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-09-28 11:41:56 +0530

    n = int(input()) m = int(input()) for i in range(n,m+1):     c=0     for j in range(2,int(i**0.5)+1):         if i%j==0:             c+=1     if c==0 and i!=1:         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,12 +1,9 @@
-n=int(input())
-m=int(input())
-l=[]
-for i in range(n+1,m+1):
+n = int(input())
+m = int(input())
+for i in range(n,m+1):
     c=0
-    for j in range(2,(i//2)+1):
+    for j in range(2,int(i**0.5)+1):
         if i%j==0:
             c+=1
-    if c==0:
-        l.append(i)
-for k in range(len(l)):
-    print(l[k])
+    if c==0 and i!=1:
+        print(i)
