45fc347640fea26a827a36689b39c34db778a403
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-09 13:13:23 +0530

    s=input() s=s.lower() k=list(set(s)) k.sort() for i in k:     if i==" ":         del k[k.index(i)] print("".join(k))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,15 +1,8 @@
-s = input().lower()
-a = ''
-for i in set(s):
-    if s.count(i)==1:
-        a+=i
-a = list(a)
-a.sort()
-a = str(a)
-a=a.replace(" ","")
-a=a.replace("'","")
-a=a.replace("[","")
-a=a.replace("]","")
-a=a.replace(" ","")
-a=a.replace(",","")
-print(a)
+s=input()
+s=s.lower()
+k=list(set(s))
+k.sort()
+for i in k:
+    if i==" ":
+        del k[k.index(i)]
+print("".join(k))
