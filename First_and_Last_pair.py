5bb07b26bcb96950a08a466a035c945562eee8a6
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-14 15:33:47 +0530

    n = int(input()) arr = list(map(int,input().split())) if n%2:     f = arr[0:(n//2)+1]     l = arr[n-1:n//2:-1] else:     f = arr[0:(n//2)]     l = arr[n-1:n//2-1:-1] if n%2:     l.append(0) for i in range(len(f)):     print(f[i],l[i],end=" ")

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -1,12 +1,13 @@
-n=int(input())
-a=list(map(int,input().split()))
-i=0
-b=[]
-while i!=n//2:
-    b.append(a[i])
-    b.append(a[(n-1)-i])
-    i+=1
-if len(a)%2!=0:
-    b.append(a[i])
-    b.append(0)
-print(*b)
+
+n = int(input())
+arr = list(map(int,input().split()))
+if n%2:
+    f = arr[0:(n//2)+1]
+    l = arr[n-1:n//2:-1]
+else:
+    f = arr[0:(n//2)]
+    l = arr[n-1:n//2-1:-1]
+if n%2:
+    l.append(0)
+for i in range(len(f)):
+    print(f[i],l[i],end=" ")
