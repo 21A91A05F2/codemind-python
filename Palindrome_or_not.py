6f1f543d3d560f682004dedef6125f2bb1ccadf3
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-14 16:06:55 +0530

    n = input() #print(n) i = 0 j = len(n)-1 count = 0 n = n.lower() #print(n) if len(n)%2!=0:     while i<=j:         if n[i]==n[j]:             count += 1             #print(n[i],n[j])         i += 1         j -= 1     if count==len(n)//2+1:         print(True)     else:         print(False) else:     while i<j:         if n[i]==n[j]:             count += 1             #print(n[i],n[j])         i += 1         j -= 1     if count==len(n)//2:         print(True)     else:         print(False)

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,6 +1,29 @@
-s=input()
-ss=s[::-1]
-if s.lower()==ss.lower():
-    print(True)
+n = input()
+#print(n)
+i = 0
+j = len(n)-1
+count = 0
+n = n.lower()
+#print(n)
+if len(n)%2!=0:
+    while i<=j:
+        if n[i]==n[j]:
+            count += 1
+            #print(n[i],n[j])
+        i += 1
+        j -= 1
+    if count==len(n)//2+1:
+        print(True)
+    else:
+        print(False)
 else:
-    print(False)
+    while i<j:
+        if n[i]==n[j]:
+            count += 1
+            #print(n[i],n[j])
+        i += 1
+        j -= 1
+    if count==len(n)//2:
+        print(True)
+    else:
+        print(False)
