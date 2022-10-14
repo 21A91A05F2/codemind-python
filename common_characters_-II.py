083ff22013165b4cd07078cdc6e0b9cd32464055
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-14 15:56:16 +0530

    s1 = input().lower() s2 = input().lower() s1,s2 = s1.replace(" ",""),s2.replace(" ","") a = [] for i in s1:     if i in s2 and i not in a:         a.append(i) print(len(a))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -1,11 +1,8 @@
-s1=input()
-s2=input()
-s1=s1.lower()
-s2=s2.lower()
-c=0
-s=""
+s1 = input().lower()
+s2 = input().lower()
+s1,s2 = s1.replace(" ",""),s2.replace(" ","")
+a = []
 for i in s1:
-    if i in s2 and i not in s and i!=" ":
-        s+=i
-        c+=1
-print(c)
+    if i in s2 and i not in a:
+        a.append(i)
+print(len(a))
