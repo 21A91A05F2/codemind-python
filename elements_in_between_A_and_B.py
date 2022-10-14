fffbc7a6ae1faa7bea30abe9c27e5e9ff3bfe65b
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-14 15:16:34 +0530

    n = int(input()) arr = list(map(int,input().split())) a,b = map(int,input().split()) c =[] for i in range(n):     if arr[i]>=a and arr[i]<=b:        c.append(arr[i]) if len(c)==0:     print("-1") else:     print(*c)

--- elements_in_between_A_and_B.py
+++ elements_in_between_A_and_B.py
@@ -1,11 +1,12 @@
+
 n = int(input())
 arr = list(map(int,input().split()))
 a,b = map(int,input().split())
-p=[]
+c =[]
 for i in range(n):
     if arr[i]>=a and arr[i]<=b:
-        p.append(arr[i])
-if len(p)==0:
+       c.append(arr[i])
+if len(c)==0:
     print("-1")
 else:
-    print(*p)
+    print(*c)
