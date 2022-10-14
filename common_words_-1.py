e93b33b6b134829895fa4eee0641879c9df2e0b0
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-14 16:02:51 +0530

    s1 = input().lower() s2 = input().lower() s1,s2=s1.split(" "),s2.split(" ") c = 0 for i in s1:     if i in s2:         c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,9 +1,8 @@
-s1=input()
-s2=input()
-l1=list(s1.lower().split(" "))
-l2=list(s2.lower().split(" "))
-c=0
-for i in l1:
-    if i in l2 :
+s1 = input().lower()
+s2 = input().lower()
+s1,s2=s1.split(" "),s2.split(" ")
+c = 0
+for i in s1:
+    if i in s2:
         c+=1
 print(c)
