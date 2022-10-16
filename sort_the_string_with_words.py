0c72fdec4d52cc959b76f807a38c6c40f823137d
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-16 12:22:45 +0530

    n = list(map(str,input().split())) for i in range(0,len(n)):     for j in range(0,len(n)):         if i!=j and n[i]<n[j]:             temp=n[i]             n[i]=n[j]             n[j]=temp print(*n)

--- sort_the_string_with_words.py
+++ sort_the_string_with_words.py
@@ -1,4 +1,8 @@
-s=input()
-l=list(s.split(" "))
-l.sort()
-print(*l)
+n = list(map(str,input().split()))
+for i in range(0,len(n)):
+    for j in range(0,len(n)):
+        if i!=j and n[i]<n[j]:
+            temp=n[i]
+            n[i]=n[j]
+            n[j]=temp
+print(*n)
