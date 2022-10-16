5d01ef64f2038d3097df1db6509c4d824f5c1ece
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-10-16 12:25:08 +0530

    s1 = input().lower() s2 = input().lower() s1 = set(s1) s2 = set(s2) s1 = str(s1) s2 = str(s2) s1=s1.replace(" ",'') s2=s2.replace(" ",'') s1=s1.replace("[",'') s2=s2.replace("[",'') s1=s1.replace("]",'') s2=s2.replace("]",'') s1=s1.replace(",",'') s2=s2.replace(",",'') s1=s1.replace("'",'') s2=s2.replace("'",'') s1=s1.replace(" ",'') s2=s2.replace(" ",'') c=0 for i in s1:     if i not in s2:         c+=1 for i in s2:     if i not in s1:         c+=1 print(c)

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,15 +1,26 @@
-s1=input()
-s2=input()
-s1=s1.lower()
-s2=s2.lower()
-s=""
+s1 = input().lower()
+s2 = input().lower()
+s1 = set(s1)
+s2 = set(s2)
+s1 = str(s1)
+s2 = str(s2)
+s1=s1.replace(" ",'')
+s2=s2.replace(" ",'')
+s1=s1.replace("[",'')
+s2=s2.replace("[",'')
+s1=s1.replace("]",'')
+s2=s2.replace("]",'')
+s1=s1.replace(",",'')
+s2=s2.replace(",",'')
+s1=s1.replace("'",'')
+s2=s2.replace("'",'')
+s1=s1.replace(" ",'')
+s2=s2.replace(" ",'')
 c=0
 for i in s1:
-    if i not in s2 and i not in s and i!=" ":
-        s+=i
+    if i not in s2:
         c+=1
 for i in s2:
-    if i not in s1 and i not in s and i!=" ":
-        s+=i
+    if i not in s1:
         c+=1
 print(c)
